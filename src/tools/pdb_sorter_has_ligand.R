c6f5b55a54a8e2a8a848593aeac13e18c8800c13
Melanie Schneider <melanie@ebi.ac.uk>
2024-04-10 14:10:21 +0100

    bug fix for output directory argument

--- src/tools/pdb_sorter_has_ligand.R
+++ src/tools/pdb_sorter_has_ligand.R
@@ -28,7 +28,7 @@ if (opt$outdir == "outdir"){
   sub_dir <- strsplit(opt$outdir, split='./', fixed=TRUE)
   outdir <- file.path(getwd(), sub_dir)
   dir.create(outdir, showWarnings = FALSE)
-} else if (length(strsplit(opt$outdir, split='/', fixed=TRUE)) == 1){
+} else if (length(unlist(strsplit(opt$outdir, split='/', fixed=TRUE))) == 1){
   # if only output foldername is provided use current working directory as basepath
   outdir <- file.path(getwd(), opt$outdir)
   dir.create(outdir, showWarnings = FALSE)
@@ -49,19 +49,16 @@ if (dir.exists(file.path(opt$indir))){ #if (!is.null(opt$indir)){
     indir <- file.path(getwd(), opt$indir)
   }
   print(indir)
+  # get pdb files in list
   files <- list.files(path = indir, pattern = "*.pdb", full.names = T, recursive = F)
-  # } else if (!is.null(opt$filenames)) {
-  #   files <- as.list(strsplit(opt$filenames, ",")[[1]])
-  #   #files <- opt$filenames
-  #   setwd(paste(dirname(files[1]),opt$outdir))
 } else {
     print_help(opt_parser)
     stop("At least one input argument must be supplied (input filepath or files).\n\n", call.=FALSE)
 }
 
-print(indir)
+#print(indir)
+#print(outdir)
 setwd(outdir)
-print(outdir)
 
 
 ## program...
