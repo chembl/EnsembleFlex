ed6e2005eec94e1cac3f17a62333873d9270e13a
Melanie Schneider <melanie@ebi.ac.uk>
2024-07-12 10:53:44 +0100

    added option to delete negative-0 residues from all pdbs (that cause issues for ligand binding pattern analysis)

--- src/tools/run_pdb_del_on_directory.sh
+++ src/tools/run_pdb_del_on_directory.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Runs the specified pdb_delXXX.py script on all pdb files in a specified directory
-# usage: run_pdb_del_on_directory.sh <path_to_pdb_del_script> <path_to_directory> <option> <outpath(optional)>
-# to be run with scripts pdb_delelem.py, pdb_delresname.py, pdb_delhetatm.py, and pdb_delhetatm_ions.py
+# Usage: run_pdb_del_on_directory.sh <path_to_pdb_del_script> <path_to_directory> <option> <outpath(optional)>
+
+# to be run with scripts pdb_delelem.py, pdb_delresname.py, pdb_delhetatm.py, pdb_delhetatm_ions.py, and pdb_delres.py
 
 # example1: run_pdb_del_on_directory.sh ~/ARISE/ARISE-Project/EnsembleFlex/src/tools/pdb_delelem.py \
 #EnsemblFlex/superimposed MN EnsemblFlex/superimposed_no_MN
@@ -13,10 +14,15 @@
 # example3: run_pdb_del_on_directory.sh ~/ARISE/ARISE-Project/EnsembleFlex/src/tools/pdb_delhetatm_ions.py \
 #EnsemblFlex/superimposed ions EnsemblFlex/superimposed_no_ions
 
+# example4: run_pdb_del_on_directory.sh ~/ARISE/ARISE-Project/EnsembleFlex/src/tools/pdb_delres.py \
+#EnsemblFlex/split_pdbs_filtered negative EnsemblFlex/superimposed_no_negative
+
+
 # path_to_script=$1
 # path_to_directory=$2
 # option=$3
 # outpath=$4 (optional)
+
 
 if [ "$#" -eq 4 ]; then # if number of arguments equals 4
   outpath=$4
@@ -29,6 +35,11 @@ mkdir $outpath
 if [ "$3" = "ions" ] || [ "$3" = "hetatm" ]; then # if <option> argument is "ions" or "hetatm"
   for f in $2/*.pdb; do
     python $1 "$f" > "$outpath/$(basename "$f")" # <option> argument is not used in execution
+  done
+elif [ "$3" = "negative" ]; then # if <option> argument is "negative"
+  for f in $2/*.pdb; do
+    # <option> argument "negative" is not used in execution, instead -:0 (which deletes residues from START to 0)
+    python $1 -:0 "$f" > "$outpath/$(basename "$f")"
   done
 else
   for f in $2/*.pdb; do
